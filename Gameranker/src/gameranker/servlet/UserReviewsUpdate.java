9b206302b829bd0963f1d39b2af17dd239495e00
thomasdiaz1 <46580678+thomasdiaz1@users.noreply.github.com>
2020-03-22 16:37:07 -0400

    UserReviewsUpdate in progress

---
+++ Gameranker/src/gameranker/servlet/UserReviewsUpdate.java
@@ -0,0 +1,11 @@
+package gameranker.servlet;
+
+import javax.servlet.annotation.WebServlet;
+
+
+//update score
+//get review by id
+@WebServlet("/userreviewsupdate")
+public class UserReviewsUpdate {
+
+}
