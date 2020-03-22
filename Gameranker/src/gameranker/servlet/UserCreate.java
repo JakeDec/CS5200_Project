06ef25806d5a9c1a7a529fd3c422100d52bfdfa4
Jake Dec <dec.j@husky.neu.edu>
2020-03-22 19:53:25 -0400

    User JSP Complete

--- Gameranker/src/gameranker/servlet/UserCreate.java
+++ Gameranker/src/gameranker/servlet/UserCreate.java
@@ -16,48 +16,58 @@ import gameranker.model.Users;
 
 @WebServlet("/usercreate")
 public class UserCreate extends HttpServlet  {
-	
-protected UsersDao usersDao;
-	
+
+	protected UsersDao usersDao;
+
 	@Override
 	public void init() throws ServletException {
 		usersDao = UsersDao.getInstance();
 	}
-	
+
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// Map for storing messages.
-        Map<String, String> messages = new HashMap<String, String>();
-        req.setAttribute("messages", messages);
-        //Just render the JSP.   
-        req.getRequestDispatcher("/UserCreate.jsp").forward(req, resp);
+		Map<String, String> messages = new HashMap<String, String>();
+		req.setAttribute("messages", messages);
+		//Just render the JSP.   
+		req.getRequestDispatcher("/UserCreate.jsp").forward(req, resp);
 	}
-	
-	@Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp)
-    		throws ServletException, IOException {
-        // Map for storing messages.
-        Map<String, String> messages = new HashMap<String, String>();
-        req.setAttribute("messages", messages);
 
-        // Retrieve and validate name.
-        String userName = req.getParameter("userName");
-        if (userName == null || userName.trim().isEmpty()) {
-            messages.put("success", "Invalid Username");
-        } else {
-        	// Create the User.
-	        try {
-	        	Users user = new Users(userName);
-	        	user = usersDao.create(user);
-	        	messages.put("success", "Successfully created " + userName);
-	        } catch (SQLException e) {
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// Map for storing messages.
+		Map<String, String> messages = new HashMap<String, String>();
+		req.setAttribute("messages", messages);
+
+		// Retrieve and validate name.
+		String userName = req.getParameter("userName");
+		String steamId = req.getParameter("steamId");
+		int steamIdInt = 0;
+		try {
+			if (steamId != null && !steamId.trim().isEmpty()) {
+				steamIdInt = Integer.parseInt(steamId);
+			}
+		if (userName == null || userName.trim().isEmpty()) {
+			messages.put("success", "Invalid Username");
+		} else {
+			// Create the User.
+			try {
+				Users user = new Users(-1, steamIdInt, userName);
+				user = usersDao.create(user);
+				messages.put("success", "Successfully created " + user);
+			} catch (SQLException e) {
 				e.printStackTrace();
 				throw new IOException(e);
-	        }
-        }
-        
-        req.getRequestDispatcher("/UserCreate.jsp").forward(req, resp);
-    }
+			}
+		}
+		} catch (NumberFormatException e) {
+            messages.put("success", "Invalid UserId");
+            messages.put("disableSubmit", "true");		
+		}
+
+		req.getRequestDispatcher("/UserCreate.jsp").forward(req, resp);
+	}
 
 }
--- Gameranker/src/gameranker/servlet/UserUpdate.java
+++ Gameranker/src/gameranker/servlet/UserUpdate.java
@@ -16,75 +16,99 @@ import gameranker.model.Users;
 
 @WebServlet("/userupdate")
 public class UserUpdate extends HttpServlet {
-	
-protected UsersDao usersDao;
-	
+
+	protected UsersDao usersDao;
+
 	@Override
 	public void init() throws ServletException {
 		usersDao = UsersDao.getInstance();
 	}
-	
+
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// Map for storing messages.
-        Map<String, String> messages = new HashMap<String, String>();
-        req.setAttribute("messages", messages);
+		Map<String, String> messages = new HashMap<String, String>();
+		req.setAttribute("messages", messages);
 
-     // Retrieve a user and validate.
-        int userId = 0;
-        if (userId == 0) {
-            messages.put("success", "Please enter a valid UserId.");
-        } else {
-        	try {
-        		userId = Integer.valueOf(req.getParameter("userid"));
-        		Users user = usersDao.getUserByUserId(userId);
-        		if(user == null) {
-        			messages.put("success", "User does not exist.");
-        		}
-        		req.setAttribute("userid", user);
-        	} catch (SQLException e) {
+		// Retrieve a user and validate.
+		int userId = 0;
+		if (userId == 0) {
+			messages.put("success", "Please enter a valid UserId.");
+		} else {
+			try {
+				userId = Integer.valueOf(req.getParameter("userid"));
+				Users user = usersDao.getUserByUserId(userId);
+				if(user == null) {
+					messages.put("success", "User does not exist.");
+				}
+				req.setAttribute("userid", user);
+			} catch (SQLException e) {
 				e.printStackTrace();
 				throw new IOException(e);
-	        }
-        }
-        
-        req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
+			}
+		}
+
+		req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
 	}
-	
-	@Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp)
-    		throws ServletException, IOException {
-        // Map for storing messages.
-        Map<String, String> messages = new HashMap<String, String>();
-        req.setAttribute("messages", messages);
 
-        // Retrieve a user and validate.
-        int userId = Integer.valueOf(req.getParameter("userid"));
-        if (userId == 0) {
-            messages.put("success", "Please enter a valid UserId.");
-        } else {
-        	try {
-        		Users user = usersDao.getUserByUserId(userId) ;
-        		if(user == null) {
-        			messages.put("success", "User does not exist. No update to perform.");
-        		} else {
-        			String newUserName = req.getParameter("newusername");
-        			if (newUserName == null || newUserName.trim().isEmpty()) {
-        	            messages.put("success", "Please enter a valid UserName.");
-        	        } else {
-        	        	user = usersDao.setUserName(user, newUserName);
-        	        	messages.put("success", "Successfully updated " + user.getUserName());
-        	        }
-        		}
-        		req.setAttribute("userid", user);
-        	} catch (SQLException e) {
-				e.printStackTrace();
-				throw new IOException(e);
-	        }
-        }
-        
-        req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
-    }
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// Map for storing messages.
+		Map<String, String> messages = new HashMap<String, String>();
+		req.setAttribute("messages", messages);
+
+		// Retrieve a user and validate.
+		int userIdInt = 0;
+		int steamIdInt = 0;
+		
+		
+		String userName = req.getParameter("newusername");
+		String userId = req.getParameter("userid");
+		String steamId = req.getParameter("newSteamId");
+
+		try {
+			if (!userId.trim().isEmpty()) {
+				userIdInt = Integer.parseInt(userId);
+			}
+			if (!steamId.trim().isEmpty()) {
+				steamIdInt = Integer.parseInt(steamId);
+			}
+		} catch (NumberFormatException e) {
+			messages.put("success", "Please enter a valid ID.");
+			req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
+			return;
+			//Pass
+		}
+
+		if (userIdInt == 0) {
+			messages.put("success", "Please enter a valid UserId.");
+			req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
+			return;
+		}
+
+		try {
+			Users user = usersDao.getUserByUserId(userIdInt);
+			String userString = user.toString();
+			if(user == null) {
+				messages.put("success", "User does not exist. No update to perform.");
+				req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
+				return;
+			} 
+			if (!userName.trim().isEmpty()) {
+				UsersDao.getInstance().setUserName(user, userName);
+			}
+			if (steamIdInt != 0) {
+				UsersDao.getInstance().setSteamId(user, steamIdInt);
+			}
+			req.setAttribute("userid", user);
+			messages.put("success", String.format("Updated %s \nto %s", userString, user.toString()));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new IOException(e);
+		}
+		req.getRequestDispatcher("/UserUpdate.jsp").forward(req, resp);
+	}
 
 }
