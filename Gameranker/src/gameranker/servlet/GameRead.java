95a5e683fab2230b3299eda1fe0c5e396333eab3
thomasdiaz1 <46580678+thomasdiaz1@users.noreply.github.com>
2020-03-22 20:37:30 -0400

    now works

--- Gameranker/src/gameranker/servlet/GameRead.java
+++ Gameranker/src/gameranker/servlet/GameRead.java
@@ -2,7 +2,9 @@ package gameranker.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import gameranker.dal.GamesDao;
 import gameranker.model.Games;
+import gameranker.model.Publishers;
 
 @WebServlet("/gameread")
 public class GameRead extends HttpServlet{
@@ -46,8 +49,13 @@ protected GamesDao gamesDao;
     			throw new IOException(e);
             }
         	messages.put("success", "Displaying results for " + game.getGameName());
+        	
+        	List<Games> games = new ArrayList<Games>();
+         	games.add(game);
+         	
+             
+             req.setAttribute("games", games);
         }
-        req.setAttribute("games", gameName);
         
         req.getRequestDispatcher("/GameRead.jsp").forward(req, resp);
 	}
@@ -72,9 +80,15 @@ protected GamesDao gamesDao;
     			e.printStackTrace();
     			throw new IOException(e);
             }
-        	messages.put("success", "Displaying results for " + game.getGameName());
+        	messages.put("success", "Displaying results for " + gameName);
+        
+        	List<Games> games = new ArrayList<Games>();
+         	games.add(game);
+         	
+             
+             req.setAttribute("games", games);
         }
-        req.setAttribute("games", gameName);
+
         
         req.getRequestDispatcher("/GameRead.jsp").forward(req, resp);
     }
